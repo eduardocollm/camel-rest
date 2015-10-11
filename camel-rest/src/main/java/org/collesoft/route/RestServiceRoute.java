cfb242a78580d96df49a7bf6d669fff41cf0a00b
eduardocollm <coll.eduardo@gmail.com>
2015-10-11 16:18:35 -0500

    tesing different config options

--- camel-rest/src/main/java/org/collesoft/route/RestServiceRoute.java
+++ camel-rest/src/main/java/org/collesoft/route/RestServiceRoute.java
@@ -1,6 +1,7 @@
 package org.collesoft.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,8 @@ public class RestServiceRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		
+		restConfiguration().component("restlet").host("localhost").bindingMode(RestBindingMode.auto);
 		
 		rest("/api").get("/start").to("direct:startProcess").get("/stop").to("direct:stopProcess");
 
